ba1ba8ec1d52c9e358fa1c9ce691bb109588ca47
95Rajitha <95rajithalbandara@gmail.com>
2021-02-18 11:48:06 +0530

    impl of using webclient demo

--- movie_catalog_service/src/main/java/com/example/movie_catalog_service/resources/MovieCatalogResources.java
+++ movie_catalog_service/src/main/java/com/example/movie_catalog_service/resources/MovieCatalogResources.java
@@ -8,9 +8,10 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
-import java.util.Collections;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,9 @@ public class MovieCatalogResources
 {
     @Autowired
     private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("/{userId}")
     public List<CatalogItem> getCatalog( @PathVariable("userId")  String userId ){
@@ -31,7 +35,21 @@ public class MovieCatalogResources
 
        return ratings.stream().map( rating ->
        {
-           Movie movie = restTemplate.getForObject( "http://localhost:8081/movies/"+rating.getMovieId(),Movie.class );
+           //deprecating the restTemplate to use webClient
+//           Movie movie = restTemplate.getForObject( "http://localhost:8081/movies/"+rating.getMovieId(),Movie.class );
+
+          Movie movie = webClientBuilder.build()
+                           .get()
+                           .uri( "http://localhost:8081/movies/"+rating.getMovieId() )
+                           .retrieve()
+                           .bodyToMono( Movie.class )
+                           .block();
+            //.get that refers to what the call is going to d
+           //url url that u need to access
+           //.retrieve go do the fetch
+           //.bodytomono what ever the body you get convert to the instance of the class
+            //.block wait till the thing finish
+
 
            return new CatalogItem( movie.getName(), "released on 2005", rating.getRating());
        }).collect( Collectors.toList());
